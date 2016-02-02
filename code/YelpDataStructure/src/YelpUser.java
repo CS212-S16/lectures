058faef03b0320f07987385250f15f06b0100b6c
Sami Rollins <srollins@cs.usfca.edu>
2016-02-01 16:59:42 -0800

    adding data structures example

---
+++ code/YelpDataStructure/src/YelpUser.java
@@ -0,0 +1,20 @@
+/*
+{  
+   "votes":{  
+      "funny":15,
+      "useful":48,
+      "cool":18
+   },
+   "user_id":"JkeCKyEaQlbLd9uZYl4DjA",
+   "name":"LiiLii C.",
+   "url":"http://www.yelp.com/user_details?userid=JkeCKyEaQlbLd9uZYl4DjA",
+   "average_stars":3.2258064516128999,
+   "review_count":31,
+   "type":"user"
+}
+ */
+
+public class YelpUser {
+
+		
+}
---
+++ code/YelpDataStructure/src/YelpUsers.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class YelpUsers {
+
+	//Maintain a data structure containing data about all Yelp users.
+	//It should be efficient to fetch user information given a specific user id.
+	//It should also be efficient to fetch a list of all users with a given user name.
+	
+	
+	public YelpUsers() {
+	}	
+	
+	
+	public void add(YelpUser user) {
+		
+	}
+	
+	public ArrayList<YelpUser> getByName(String name) {
+		
+		return null;
+	}
+	
+	public YelpUser getByUserId(String userId) {
+		
+		return null;
+	}
+	
+	
+	
+}
