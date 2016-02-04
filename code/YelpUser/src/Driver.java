fbf3b5a92aef794f58c5b7d5685c58fcf11521cf
Sami Rollins <srollins@cs.usfca.edu>
2016-02-04 15:50:40 -0800

    removing unnecessary projects

--- code/YelpUser/src/Driver.java
+++ code/YelpUser/src/Driver.java
@@ -1,10 +1,12 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.json.simple.parser.JSONParser;
+
 public class Driver {
 
 	public static void main(String[] args) {
-
+		
 		Path path = Paths.get("input/user_dataset.json");
 		YelpUserStatistics stats = new YelpUserStatistics(path);
 		System.out.printf("Active users: %d\n", stats.getNumberActiveUsers());
